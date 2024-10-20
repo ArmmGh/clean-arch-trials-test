35d0a615dc3072de13145c4a3c8aa8b3e107d827
armmgh <arm.ghazaryan01@gmail.com>
2024-10-20 19:05:53 +0400

    test cookie header

--- src/app/dashboard/create-article/page.tsx
+++ src/app/dashboard/create-article/page.tsx
@@ -1,10 +1,12 @@
 import getPublisherAllChannelsAction from '@/app/actions/getPublisherAllChannels.action'
 import Main from '@/components/layout/Main'
 import { getPublisherAddressFromSession } from '@/lib/utils/getPublisherAddressFromSession'
+import { cookies } from 'next/headers'
 import CreateArticleFlow from './components/CreateArticleFlow'
 
 export default async function CreateArticle() {
-  const publisherAddress = getPublisherAddressFromSession()
+  const cookiesData = cookies()
+  const publisherAddress = getPublisherAddressFromSession(cookiesData)
   const channels = await getPublisherAllChannelsAction({ publisherAddress })
 
   return (
