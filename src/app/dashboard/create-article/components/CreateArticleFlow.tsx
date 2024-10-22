2713fafff7e16b9e58ad3ce23fc0e516cf68f6a7
Arman Ghazaryan <arm.ghazaryan01@gmail.com>
2024-10-22 14:44:14 +0400

    added feat to prevent unnecesary reloads from create-article, fixed address change state persistance on article create (#31) (#32)

--- src/app/dashboard/create-article/components/CreateArticleFlow.tsx
+++ src/app/dashboard/create-article/components/CreateArticleFlow.tsx
@@ -4,20 +4,47 @@ import WithAuth from '@/components/HOC/withAuth'
 import NoChannels from '@/components/NoChannels'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import type { Channel as ChannelType } from '@/entities/models/channel'
-import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { useAccount } from 'wagmi'
 import ArticleCreationStepper from './ArticleCreationStepper'
 import ChannelChooser from './ChannelChooser'
 import DraftedArticles from './DraftedArticles'
 
 function CreateArticleFlow({ channels }: { channels: ChannelType[] }) {
+  const { address } = useAccount()
   const [activeChannelAddress, setActiveChannelAddress] = useState<ChannelType['address']>()
   const [tab, setTab] = useState<'create' | 'drafts'>('create')
+  const router = useRouter()
 
   const onTabChange = (value: string) => {
     setTab(value as 'create' | 'drafts')
   }
 
-  if (channels.length === 0) return <NoChannels />
+  useEffect(() => {
+    if (!channels.length) return
+
+    const onBeforeUnload = () => {
+      return
+    }
+
+    window.addEventListener('beforeunload', onBeforeUnload)
+
+    return () => {
+      window.removeEventListener('beforeunload', onBeforeUnload)
+    }
+  }, [])
+
+  useEffect(() => {
+    router.refresh()
+  }, [address])
+
+  if (channels.length === 0)
+    return (
+      <div className='py-4'>
+        <NoChannels />
+      </div>
+    )
 
   return (
     <div className='flex flex-col items-center py-4'>
